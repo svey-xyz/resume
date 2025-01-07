1874912fd0957f626129bde25223b6e0d4388ed6
svey-xyz <x@svey.xyz>
2025-01-07 14:59:13 -0500

    Format content

--- app/page.tsx
+++ app/page.tsx
@@ -3,56 +3,65 @@
 const Home = () => {
   return (
 		<section className="">
-			<h1 id="hayden-soule"><strong>Hayden Soule</strong></h1>
+			<h1>Hayden Soule</h1>
 			<p><a href="mailto:x@svey.xyz">x@svey.xyz</a> | <a href="https://svey.xyz" title="Personal Site">svey.xyz</a> | <a href="https://github.com/svey-xyz" title="Personal GitHub">github.com/svey-xyz</a> | <a href="tel:+16138067643">(613) 806-7643</a></p>
 			<p>I am a full stack web developer, and homelab enthusiast, working to create robust web experiences. My unique background in tech and the arts inﬂuences my approach to projects and leads to innovative solutions.</p>
+			
 			<hr />
-				<h2 id="professional-experience"><strong>Professional Experience</strong></h2>
-				<h3 id="open-source-contributions">Open Source Contributions</h3>
-				<p><em>Freelance</em> | <em>May 2022 - Present</em></p>
-				<ul>
-					<li>Created &amp; maintain <a href="https://www.npmjs.com/package/@svey-xyz/simple-shader-component">Simple Shader Component</a> - a simple to use webgl shader component, with included framework wrappers and strong typing.</li>
-					<li>Created <a href="https://www.npmjs.com/package/vldx">vldx</a> -  an open-srouce javascript based HTML form validator that's easy to use, reduces API calls, and supports custom rules.</li>
-					<li>Open sourced my personal <a href="https://github.com/svey-xyz/site-template">Next.js template</a> - with highly configurable base components and schemas.</li>
-					<li>Received positive community &amp; maintainer feedback when <a href="https://github.com/ndimatteo/sanity-plugin-note-field/pull/5">contributing to an open source Sanity plugin</a>.</li>
-				</ul>
-				<h3 id="lecturing-in-web-design">Lecturing in Web Design</h3>
-				<p><em>Freelance</em> | <em>January 2021 - Present</em></p>
-				<ul>
-					<li>Created an introduction to web design workshop for a class of 4th year students at TMU. A 3 hour lexture with hands-on compoennts that has been highly requested by students and proffessors. 2025 will mark my 4th year running of conducting this workshop at TMU.</li>
-					<li>Positive feedback from TMU lead to the creation of an asynchronous Wordpress workshop for the University of North Carolina at Greensboro (UNCG), that has been used to teach a large number of students over the years.</li>
-					<li>Was invited to present artist talks at TMU and UNCG, on- web design, web development, interactive web installations, and working as a contractor.</li>
-				</ul>
-				<h3 id="web-development-freelance--march-2020---present">Web Development <em>Freelance</em> | <em>March 2020 - Present</em></h3>
-				<ul>
-					<li>Clients include <a href="https://victorysocialclub.com/">Victory Social Club (VSC)</a>, <a href="https://lptbia.com/">LPT BIA</a>, Toronto Metropolitan University (TMU), the city of Toronto, and more.</li>
-					<li>Reduced overhead by creating bespoke management interfaces using Sanity and Nextjs; resulting in hours of work saved for clients.</li>
-					<li>Increased Lighthouse scores by up to 30pts by reducing CSR and SSR where applicable in favour of SSG.</li>
-					<li>Provided intuitive documentation for a custom client interface, making onboarding new developers to the project easier.</li>
-				</ul>
-				<h3 id="interactive-art-exhibition">Interactive Art Exhibition</h3>
-				<p><em>Freelance</em> | <em>September 2021 - December 2021</em></p>
-				<ul>
-					<li>
-						<p>Designed and developed a large scale public exhibition. Created interactive systems for participants to engage with the work.</p>
-					</li>
-					<li>
-						<p>Increased participation with the exhibition by clearly communicating the multifaceted thoughts of an interdisciplinary team on the web.</p>
-						<p>what I did, what tech i used, for whom, to what end - to high degress of client satisfaction</p>
-						<p>what and why</p>
-						<p>include links</p>
-					</li>
-				</ul>
-				<hr />
-					<h2 id="education"><strong>Education</strong></h2>
-					<h3 id="bachelors-degree-of-fine-arts-with-a-specialization-in-integrated-digital-and-with-distinction">Bachelor's Degree of Fine Arts with a specialization in Integrated Digital and with distinction</h3>
-					<p><em>Toronto Metropolitan University</em> | <em>2020</em>
-						relevant courses, grades
-						thesis - 2d platformer</p>
-					<hr />
-						<h2 id="skills"><strong>Skills</strong></h2>
-						<p>Linux | Virtualization | Containerization | Git | Network Infrastructure | Relational Databases | SQL | REST | GraphQL | Web Development | Unit testing | JavaScript | TypeScript | Node.js | Bun  | PHP | Wordpress | React | Nextjs</p>
-						<p>Projects  - homelab / private cloud what skills were used</p>
+
+			<h2><strong>Professional Experience</strong></h2>
+			<h3>Open Source Contributions</h3>
+			<p><em>Freelance</em> | <em>May 2022 - Present</em></p>
+			<ul>
+				<li>Created &amp; maintain <a href="https://www.npmjs.com/package/@svey-xyz/simple-shader-component">Simple Shader Component</a> - a simple to use webgl shader component, with included framework wrappers and strong typing.</li>
+				<li>Created <a href="https://www.npmjs.com/package/vldx">vldx</a> -  an open-srouce javascript based HTML form validator that's easy to use, reduces API calls, and supports custom rules.</li>
+				<li>Open sourced my personal <a href="https://github.com/svey-xyz/site-template">Next.js template</a> - with highly configurable base components and schemas.</li>
+				<li>Received positive community &amp; maintainer feedback when <a href="https://github.com/ndimatteo/sanity-plugin-note-field/pull/5">contributing to an open source Sanity plugin</a>.</li>
+			</ul>
+
+			<h3>Lecturing in Web Design</h3>
+			<p><em>Freelance</em> | <em>January 2021 - Present</em></p>
+			<ul>
+				<li>Created an introduction to web design workshop for a class of 4th year students at TMU. A 3 hour lexture with hands-on compoennts that has been highly requested by students and proffessors. 2025 will mark my 4th year running of conducting this workshop at TMU.</li>
+				<li>Positive feedback from TMU lead to the creation of an asynchronous Wordpress workshop for the University of North Carolina at Greensboro (UNCG), that has been used to teach a large number of students over the years.</li>
+				<li>Was invited to present artist talks at TMU and UNCG, on- web design, web development, interactive web installations, and working as a contractor.</li>
+			</ul>
+
+			<h3>Web Development <em>Freelance</em> | <em>March 2020 - Present</em></h3>
+			<ul>
+				<li>Clients include <a href="https://victorysocialclub.com/">Victory Social Club (VSC)</a>, <a href="https://lptbia.com/">LPT BIA</a>, Toronto Metropolitan University (TMU), the city of Toronto, and more.</li>
+				<li>Reduced overhead by creating bespoke management interfaces using Sanity and Nextjs; resulting in hours of work saved for clients.</li>
+				<li>Increased Lighthouse scores by up to 30pts by reducing CSR and SSR where applicable in favour of SSG.</li>
+				<li>Provided intuitive documentation for a custom client interface, making onboarding new developers to the project easier.</li>
+			</ul>
+
+			<h3>Interactive Art Exhibition</h3>
+			<p><em>Freelance</em> | <em>September 2021 - December 2021</em></p>
+			<ul>
+				<li>
+					<p>Designed and developed a large scale public exhibition. Created interactive systems for participants to engage with the work.</p>
+				</li>
+				<li>
+					<p>Increased participation with the exhibition by clearly communicating the multifaceted thoughts of an interdisciplinary team on the web.</p>
+					<p>what I did, what tech i used, for whom, to what end - to high degress of client satisfaction</p>
+					<p>what and why</p>
+					<p>include links</p>
+				</li>
+			</ul>
+
+			<hr />
+
+			<h2>Education</h2>
+			<h3>Bachelor's Degree of Fine Arts with a specialization in Integrated Digital and with distinction</h3>
+			<p><em>Toronto Metropolitan University</em> | <em>2020</em>
+				relevant courses, grades
+				thesis - 2d platformer</p>
+
+			<hr />
+
+			<h2>Skills</h2>
+			<p>Linux | Virtualization | Containerization | Git | Network Infrastructure | Relational Databases | SQL | REST | GraphQL | Web Development | Unit testing | JavaScript | TypeScript | Node.js | Bun  | PHP | Wordpress | React | Nextjs</p>
+			<p>Projects  - homelab / private cloud what skills were used</p>
 		</section>
   );
 }
