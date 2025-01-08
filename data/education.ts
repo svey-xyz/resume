8cdaeabc343bacf54b5fa8f936368641b98e38cc
svey-xyz <x@svey.xyz>
2025-01-07 23:05:20 -0500

    Education data

--- data/education.ts
+++ data/education.ts
@@ -9,7 +9,8 @@ const education: Array<Experience> = [
 			}
 		},
 		points: [
-			
+			`4.01/4.33 GPA, Dean's List, Distinction`,
+			`[Thesis](https://github.com/svey-xyz/oltvara-thesis) - an infinitely generated 2D platformer. Built with Godot and Java.`
 		]
 	},
 ]
